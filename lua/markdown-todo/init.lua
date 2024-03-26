f184f766fb99198d5fbe809636f5a00edcd740c6
LVIM ORG <lvim@motion-agency.com>
2024-03-26 11:34:09 +0200

    Rewrite init

--- lua/markdown-todo/init.lua
+++ lua/markdown-todo/init.lua
@@ -17,7 +17,7 @@ local lead_chars = {
 --- Returns the start and end index of the lead character if found, or nil otherwise.
 ---@param line string
 ---@return number|nil, number|nil
-local function is_lead_char(line)
+M.is_lead_char = function(line)
 	for _, lead_char in ipairs(lead_chars) do
 		local start, finish = line:find("^%s*" .. lead_char)
 		if start then
@@ -31,7 +31,7 @@ end
 --- Returns the start and end indices of the todo indicator if found, or nil otherwise.
 ---@param line string
 ---@return number|nil, number|nil
-local function has_todo_indicator(line)
+M.has_todo_indicator = function(line)
 	local start, finish = line:find("%(%s?[%s%-_=xy!+?o]%s?%)")
 	return start, finish
 end
@@ -41,8 +41,8 @@ end
 ---@param line string
 ---@param itemType TodoItemType
 ---@return string
-local function add_todo_indicator(line, itemType)
-	local start, finish = is_lead_char(line)
+M.add_todo_indicator = function(line, itemType)
+	local start, finish = M.is_lead_char(line)
 	if start then
 		line = line:sub(1, finish) .. " (" .. config.indicators[itemType].literal .. ")" .. line:sub(finish + 1)
 	end
@@ -54,8 +54,8 @@ end
 ---@param line string
 ---@param itemType TodoItemType
 ---@return string
-local function update_todo_indicator(line, itemType)
-	local todo_indicator_index = has_todo_indicator(line)
+M.update_todo_indicator = function(line, itemType)
+	local todo_indicator_index = M.has_todo_indicator(line)
 	if todo_indicator_index then
 		line = line:sub(1, todo_indicator_index - 1)
 			.. "("
@@ -71,14 +71,14 @@ local ns_id = vim.api.nvim_create_namespace("markdown-todo")
 --- Hide virtual text if todo indicator is being edited.
 --- Return line number (0 based) being edited.
 ---@return number|false
-local function should_hide_icons()
+M.should_hide_icons = function()
 	local line_num = vim.fn.line(".") - 1
 	return line_num
 end
 
 --- Clear existing extmarks
 ---@param line_num number
-local function hide_virtual_icons(line_num)
+M.hide_virtual_icons = function(line_num)
 	local extmarks = vim.api.nvim_buf_get_extmarks(0, ns_id, { line_num, 0 }, { line_num + 1, 0 }, {})
 	for _, extmark in ipairs(extmarks) do
 		vim.api.nvim_buf_del_extmark(0, ns_id, extmark[1])
@@ -89,9 +89,9 @@ end
 ---@param indicator_index number
 ---@param itemType TodoItemType
 ---@param line_num number
-local set_virtual_icon = function(indicator_index, itemType, line_num)
+M.set_virtual_icon = function(indicator_index, itemType, line_num)
 	-- before setting new icons, clear existing ones
-	hide_virtual_icons(line_num)
+	M.hide_virtual_icons(line_num)
 	vim.api.nvim_buf_set_extmark(0, ns_id, line_num, indicator_index, {
 		-- virt_text = { { config.indicators[itemType].icon, config.indicators[itemType].hl } },
 		virt_text = { { config.indicators[itemType].icon } },
@@ -101,7 +101,7 @@ local set_virtual_icon = function(indicator_index, itemType, line_num)
 end
 
 -- Binds keys to current buffer only.
-local function bind_keys()
+M.bind_keys = function()
 	-- stylua: ignore start
 	vim.keymap.set("n", config.keys.undone, function() require("markdown-todo").make_todo("undone") end, { buffer = 0, desc = "Mark as Undone" })
 	vim.keymap.set("n", config.keys.pending, function() require("markdown-todo").make_todo("pending") end, { buffer = 0, desc = "Mark as Pending" })
@@ -115,7 +115,7 @@ local function bind_keys()
 	-- stylua: ignore end
 end
 
-local function set_hl()
+M.set_hl = function()
 	for _, indicator in pairs(config.indicators) do
 		-- use \V for very nomagic (literal) matching
 		vim.fn.matchadd(indicator.hl, "(\\V" .. indicator.literal .. ")")
@@ -123,15 +123,15 @@ local function set_hl()
 end
 
 --- Scans the entire buffer for todo indicators and sets virtual icons for them.
-local function set_virtual_icons()
+M.set_virtual_icons = function()
 	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 	for i, line in ipairs(lines) do
-		local indicator_index = has_todo_indicator(line)
+		local indicator_index = M.has_todo_indicator(line)
 		if indicator_index then
 			local indicator_char = line:sub(indicator_index + 1, indicator_index + 1)
 			for itemType, indicator in pairs(config.indicators) do
 				if indicator.literal == indicator_char then
-					set_virtual_icon(indicator_index, itemType, i - 1)
+					M.set_virtual_icon(indicator_index, itemType, i - 1)
 					break
 				end
 			end
@@ -139,7 +139,7 @@ local function set_virtual_icons()
 	end
 end
 
-local function augroup(name)
+M.augroup = function(name)
 	return vim.api.nvim_create_augroup("markdown-todo_" .. name, { clear = true })
 end
 
@@ -153,70 +153,68 @@ end
 --- Returns true if the line was successfully converted into a todo item, or false otherwise.
 ---@param itemType TodoItemType
 ---@return boolean
-function M.make_todo(itemType)
+M.make_todo = function(itemType)
 	local line = vim.api.nvim_get_current_line()
-	local start = is_lead_char(line)
+	local start = M.is_lead_char(line)
 	if start then
-		if has_todo_indicator(line) then
-			line = update_todo_indicator(line, itemType)
+		if M.has_todo_indicator(line) then
+			line = M.update_todo_indicator(line, itemType)
 		else
-			line = add_todo_indicator(line, itemType)
+			line = M.add_todo_indicator(line, itemType)
 		end
 		vim.api.nvim_set_current_line(line)
-		local indicator_index = has_todo_indicator(line)
+		local indicator_index = M.has_todo_indicator(line)
 		if not indicator_index then
 			vim.api.nvim_err_writeln("Failed to add todo indicator")
 			return false
 		end
 		local line_num = vim.fn.line(".") - 1
-		set_virtual_icon(indicator_index, itemType, line_num)
+		M.set_virtual_icon(indicator_index, itemType, line_num)
 		return true
 	else
 		return false
 	end
 end
 
-function M.setup(user_config)
-	vim.schedule(function()
-		if user_config ~= nil then
-			utils.merge(config, user_config)
-		end
+M.setup = function(user_config)
+	if user_config ~= nil then
+		utils.merge(config, user_config)
+	end
 
-		-- BufWinEnter captures first window. WinEnter captures the rest.
-		vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
-			group = augroup("set_hl"),
-			pattern = { "*.md" },
-			callback = set_hl,
-		})
+	-- BufWinEnter captures first window. WinEnter captures the rest.
+	vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
+		group = M.augroup("set_hl"),
+		pattern = { "*.md" },
+		callback = M.set_hl,
+	})
 
-		-- bind keys for markdown files only
-		vim.api.nvim_create_autocmd("BufEnter", {
-			group = augroup("bind_keys"),
-			pattern = { "*.md" },
-			callback = bind_keys,
-		})
+	-- bind keys for markdown files only
+	vim.api.nvim_create_autocmd("BufEnter", {
+		group = M.augroup("bind_keys"),
+		pattern = { "*.md" },
+		callback = M.bind_keys,
+	})
 
-		-- hide virtual icons when (1) entering insert mode (2) near a todo indicator.
-		vim.api.nvim_create_autocmd({
-			"InsertEnter",
-		}, {
-			group = augroup("hide_virtual_icons"),
-			pattern = { "*.md" },
-			callback = function()
-				local line = should_hide_icons()
-				if line then
-					hide_virtual_icons(line)
-				end
-			end,
-		})
+	-- hide virtual icons when (1) entering insert mode (2) near a todo indicator.
+	vim.api.nvim_create_autocmd({
+		"InsertEnter",
+	}, {
+		group = M.augroup("hide_virtual_icons"),
+		pattern = { "*.md" },
+		callback = function()
+			local line = M.should_hide_icons()
+			if line then
+				M.hide_virtual_icons(line)
+			end
+		end,
+	})
 
-		-- always show virtual icons when leaving insert mode
-		vim.api.nvim_create_autocmd("InsertLeave", {
-			group = augroup("set_virtual_icons"),
-			pattern = { "*.md" },
-			callback = set_virtual_icons,
-		})
-	end)
+	-- always show virtual icons when leaving insert mode
+	vim.api.nvim_create_autocmd("InsertLeave", {
+		group = M.augroup("set_virtual_icons"),
+		pattern = { "*.md" },
+		callback = M.set_virtual_icons,
+	})
 end
 
 return M
